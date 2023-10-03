8492b535f3015e96cc00f2a99e55637a36b85f00
aniketkalvit <aniketkalvit@gmail.com>
2023-10-03 21:39:35 +0530

    Add project files.

---
+++ CRUDWithEF/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWithEF.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUDWithEF/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWithEF.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        // fetch the data from DB & store at application side
+        // Dbset will translate LINQ quries in the SQL & fire in the DB
+        public DbSet<Book> Books { get; set; }
+    }
+}
